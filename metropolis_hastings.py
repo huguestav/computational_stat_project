9baeb8288cab9be01d264adb4d9845ad636c563e
hugues <hugues.tavenard@eleves.enpc.fr>
2017-03-22 00:26:03 +0100

    Implement RWM_2 and add data from the article
    It seems to work but it now has to be tested on the data from the article.
    
    Langevin algorithms are yet to be implemented.

--- metropolis_hastings.py
+++ metropolis_hastings.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.stats
+import pdb
 
 
 def proposal_density(x, mean, covariance):
@@ -7,7 +8,7 @@ def proposal_density(x, mean, covariance):
 
 
 def rwm_1(initial_value, pi, sigma_2, lbda, n_steps):
-    values = np.zeros((n_steps +1, len(initial_value)))
+    values = np.zeros((n_steps + 1, len(initial_value)))
     values[0] = initial_value
     values_idx = 0
 
@@ -57,7 +58,6 @@ def is_pos_semi_def(x):
 
 def projection_2(x, A_1):
     frobenius_norm = np.linalg.norm(x, ord="fro")
-
     assert is_pos_semi_def(x)
 
     if frobenius_norm <= A_1:
@@ -74,4 +74,80 @@ def projection_3(x, A_1):
     else:
         return x * A_1 / euclidian_norm
 
-## TODO : rmw_2
+
+def rwm_2(initial_value, pi, gamma, mu_0, gamma_0, sigma_2_0, n_steps,
+          epsilon_1, epsilon_2, A_1):
+    """
+    pi and gamma are functions
+    """
+    values = np.zeros((n_steps + 1, len(initial_value)))
+    values[0] = initial_value
+    values_idx = 0
+
+    # Initialize the adaptative parameters
+    mu_n = mu_0
+    gamma_n = gamma_0
+    sigma_2_n = sigma_2_0
+
+    for i in range(n_steps):
+        current_value = values[values_idx]
+
+        # Update lambda_n
+        if i > 5000:
+            lambda_n = gamma_n + epsilon_2 * np.identity(len(initial_value))
+        else:
+            # lambda_n = gamma_0
+            lambda_n = gamma_0 + epsilon_2 * np.identity(len(initial_value))
+
+        # Step 1 : sample a candidate value
+        mean = current_value
+        covariance = sigma_2_n * lambda_n
+        candidate_value = np.random.multivariate_normal(mean=mean, cov=covariance, )
+
+        # Step 2 : accept or reject
+        alpha = (
+            pi(candidate_value)
+            / pi(current_value)
+            * proposal_density(
+                  x=current_value,
+                  mean=candidate_value,
+                  covariance=covariance)
+            / proposal_density(
+                  x=candidate_value,
+                  mean=mean,
+                  covariance=covariance)
+        )
+        alpha = min(1, alpha)
+        if np.random.rand() < alpha:
+            values_idx += 1
+            values[values_idx] = candidate_value
+            current_value = values[values_idx]
+
+        # Step 3 : update the adaptative parameters (mu_n, gamma_n, sigma_2_n)
+        mu_n_new = projection_3(
+            x=mu_n + gamma(i) * (current_value - mu_n),
+            A_1=A_1,
+        )
+        vec = (current_value - mu_n).reshape((len(current_value), 1))
+        if i > 1000:
+        # if i > 10:
+            gamma_n_new = projection_2(
+                x=gamma_n + gamma(i) * (vec.dot(vec.T) - gamma_n),
+                A_1=A_1,
+            )
+        else:
+            gamma_n_new = gamma_n
+        sigma_2_n_new = projection_1(
+            x=sigma_2_n + gamma(i) * (alpha - 0.2),
+            epsilon_1=epsilon_1,
+            A_1=A_1,
+        )
+        mu_n = mu_n_new
+        sigma_2_n = sigma_2_n_new
+        gamma_n = gamma_n_new
+
+    print("Final mu :", mu_n)
+    print("Final sigma_2 :", sigma_2_n)
+    print("Final gamma :", gamma_n)
+    return values[:values_idx+1]
+
--- test_algorithms.py
+++ test_algorithms.py
@@ -1,10 +1,13 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import pdb
 
 import metropolis_hastings
 
+# Load tmalaexcov.txt
+SIGMA = np.loadtxt("tmalaexcov.txt")
 
-## Test rwm1
+
 # Random walk metropolis parameters
 initial_value = np.array([3, -5])
 sigma_2 = 1
@@ -13,15 +16,56 @@ lbda = [[1,0], [0,1]]
 # Target density
 mu = np.array([7, 7])
 sigma = np.array([[2,0], [0,2]])
-pi = lambda x : np.exp(- 0.5 * (x - mu).T.dot(np.linalg.inv(sigma)).dot(x - mu))
+def pi(x):
+    vec = (x - mu).reshape((len(x), 1))
+    result = np.exp(- 0.5 * vec.T.dot(np.linalg.inv(sigma)).dot(vec))
+    return float(result)
 
-n_steps = 2000
-values = metropolis_hastings.rwm_1(initial_value, pi, sigma_2, lbda, n_steps)
+gamma = lambda x : 10 / (x + 1)
+delta = 1000
+epsilon_1 = 1e-7
+epsilon_2 = 1e-6
+A_1 = 1e7
+
+mu_0 = np.array([1, 1])
+gamma_0 = np.array([[1,0], [0,1]])
+sigma_2_0 = 1
+
+
+def D_mala(x):
+    sigma_inv = np.linalg.inv(sigma)
+    grad_log_pi = - sigma_inv.dot(x - mu)
+    return delta * grad_log_pi / max(delta, np.linalg.norm(grad_log_pi, ord=2))
+
+n_steps = int(20 * 1e3)
+
+
+# # Test rwm_1
+# values = metropolis_hastings.rwm_1(initial_value, pi, sigma_2, lbda, n_steps)
+# acceptance_ratio = (values.shape[0] - 1) / n_steps
+# print("Acceptance ratio :", acceptance_ratio)
+# print("mean :", np.mean(values, axis=0))
+# # print(values)
+# plt.scatter(values[:,0], values[:,1], c='b', s=20)
+# plt.show()
+
+
+# Test rwm_2
+values = metropolis_hastings.rwm_2(
+    initial_value=initial_value,
+    pi=pi,
+    gamma=gamma,
+    mu_0=mu_0,
+    gamma_0=gamma_0,
+    sigma_2_0=sigma_2_0,
+    n_steps=n_steps,
+    epsilon_1=epsilon_1,
+    epsilon_2=epsilon_2,
+    A_1=A_1,
+)
 acceptance_ratio = (values.shape[0] - 1) / n_steps
-print("Acceptance ratio :", acceptance_ratio)
-# print(values)
-
-
+print("\nAcceptance ratio :", acceptance_ratio)
+print("mean :", np.mean(values, axis=0))
 plt.scatter(values[:,0], values[:,1], c='b', s=20)
 plt.show()
 
